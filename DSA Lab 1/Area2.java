866aefc45ceb4ddea7b66bf3617ed1efd3c909cb
AliAhmed15 <49337548+AliAhmed15@users.noreply.github.com>
2019-10-24 08:23:55 +0500

    Update Area2.java

--- DSA Lab 1/Area2.java
+++ DSA Lab 1/Area2.java
@@ -1,23 +1,23 @@
 import java.util.*;
-class Area2{
+class Area2{ // class created with two variables 
 	double length;
 	double breadth;
-	Area2(double length , double breadth){
+	Area2(double length , double breadth){ //created a constructer
 		this.length=length;
 		this.breadth=breadth;
 	}
-	public double returnArea(){
+	public double returnArea(){ // reaturnArea method created eith double return class
 		return length*breadth;
 	}
 	public static void main(String []abcd){
 		
 		Scanner s=new Scanner(System.in);
 		System.out.println("Input Length");
-		double length1=s.nextDouble();
+		double length1=s.nextDouble(); //taking length input
 		System.out.println("Input Breadth");
-		double breadth1=s.nextDouble();
+		double breadth1=s.nextDouble(); //taking bradth input
 		
-		Area2 a=new Area2(length1,breadth1);
+		Area2 a=new Area2(length1,breadth1); //object of constructor created
 		
 		
 		System.out.println(a.returnArea());
