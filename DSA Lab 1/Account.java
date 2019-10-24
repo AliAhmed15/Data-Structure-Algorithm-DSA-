ff2eeb3006c02988c18772942e9af79bc48ba010
AliAhmed15 <49337548+AliAhmed15@users.noreply.github.com>
2019-10-24 08:12:39 +0500

    Update Account.java

--- DSA Lab 1/Account.java
+++ DSA Lab 1/Account.java
@@ -1,14 +1,17 @@
+//Account Class Created
 class Account{
-	protected int balance;
-	public int  withdraw(int amount){
-		this.balance = this.balance - amount;
+	protected int balance;//Integer type variable
+	public int  withdraw(int amount)//withdraw method with int return type 
+	{
+		this.balance = this.balance - amount;	
 		return amount;
 	}
 }
-class SavingAccount extends Account{
-	private double defaultIntrestRate = 2.5;
-	private double interestRate;
-	public setDefaultInterestRate(doule interestRate){
+class SavingAccount extends Account{		//savingAccount class extending Account class 
+	private double defaultIntrestRate = 2.5;		
+	private double interestRate;				
+	public setDefaultInterestRate(doule interestRate)
+	{	
 
 	}
 	public applyMonthlyInterest(){
