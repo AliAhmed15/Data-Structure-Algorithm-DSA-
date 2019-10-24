dae1ac5610ab5308525c37dfddff2025548df7c8
AliAhmed15 <49337548+AliAhmed15@users.noreply.github.com>
2019-10-24 08:29:35 +0500

    Update Employee.java

--- DSA Lab 1/Employee.java
+++ DSA Lab 1/Employee.java
@@ -1,21 +1,21 @@
 class Employee{
 	int salary;
 	int hour;
-	public void getInfo(int salary, int hour){
+	public void getInfo(int salary, int hour){ //getinfo method with salary and no. of hours variable in parameters
 		this.salary=salary;
 		this.hour=hour;
 		}
-	public void AddSal(){
+	public void AddSal(){ //AddSal method with void method and if condition
 		if(salary<500)
 			salary+=10;
 	}
-	public void AddWork(){
+	public void AddWork(){ //AddWork method eith void method and condition statement
 		if(hour>6)
 			salary+=5;
 	}
  public static void main(String []abd){
-	Employee e=new Employee();
-	e.getInfo(150, 8);
+	Employee e=new Employee(); //Employee object
+	e.getInfo(150, 8); 
 	e.AddSal();
 	e.AddWork();
 	System.out.print("Salary:"+ e.salary+"\nNo: of Hours:"+e.hour);
