63f4518c32a55dba7794295871b026d00bc9ae44
AliAhmed15 <49337548+AliAhmed15@users.noreply.github.com>
2019-10-24 08:17:04 +0500

    Update Area.java

--- DSA Lab 1/Area.java
+++ DSA Lab 1/Area.java
@@ -1,28 +1,28 @@
-import java.util.*;
-class Area{
+import java.util.*; //importing scanner class
+class Area{ 
 	double length;
 	double breadth;
 	
-	public void setDim(double length,double breadth){
+	public void setDim(double length,double breadth){ //setDim method created with no return type and two parameters for taking input
 		this.length=length;
 		this.breadth=breadth;
 	}
 	
-	public double getArea(){
+	public double getArea(){ //getArea method created eith Double return type
 		
 		return length*breadth;
 
 	}
-	public static void main(String []abcd)
+	public static void main(String []abcd) 
 	{
 		double length1,breadth1;
-		Area a=new Area();
+		Area a=new Area(); //Object of area class
 		Scanner s=new Scanner(System.in);
 		System.out.println("Input Length");
-		length1=s.nextDouble();
+		length1=s.nextDouble(); // taking length input
 		System.out.println("Input Breadth");
-		breadth1=s.nextDouble();
-		a.setDim(length1,breadth1);
+		breadth1=s.nextDouble(); // taking breadth input
+		a.setDim(length1,breadth1); //calling steDim Method using class object 
 		System.out.println(a.getArea());
 	
 	}
